c64dc9db82374d6b9b070c669a453f6ad8677a22
WOLNOME <k023g0109@g.neec.ac.jp>
2026-07-03 11:11:28 +0900

    ・サンプルシーンに当たり判定付きオブジェクトを設置

--- project/engine/scene/SampleScene.cpp
+++ project/engine/scene/SampleScene.cpp
@@ -1,5 +1,8 @@
 #include "SampleScene.h"
 
+//アプリケーション
+#include "application/object/collision/ObjectCollider.h"
+
 namespace Norm {
 
 	void SampleScene::Initialize() {
@@ -63,6 +66,34 @@ namespace Norm {
 		sampleMapWT_.SetRotate({ 0.0f,0.0f,0.0f });
 		sampleMap_->RegistWorldTransform(&sampleMapWT_);
 
+		//コライダー付きオブジェクトの生成と初期化
+		{
+			cObject_ = std::make_unique<Object3d>();
+			cObject_->Initialize(ModelTag{}, Object3dManager::GetInstance()->GenerateName("CObject"), "cube");
+
+			cObject1WT_.Initialize();
+			cObject1WT_.SetTranslate({ 0.0f,5.0f,20.0f });
+			cObject1WT_.SetRotate({ 0.0f,0.0f,0.0f });
+			cObject_->RegistWorldTransform(&cObject1WT_);
+			collider1_ = std::make_unique<ObjectCollider>(cObject_.get());
+			auto* collider1 = dynamic_cast<ObjectCollider*>(collider1_.get());
+			collider1->SetCollisionAttribute(CollisionAttribute::Player);
+			collider1->SetWorldTransform(&cObject1WT_);
+			collider1->SetOffset({ 0.0f,1.0f,0.0f });
+			collider1->SetOBBSize({ 2.0f,2.0f,2.0f });
+
+			cObject2WT_.Initialize();
+			cObject2WT_.SetTranslate({ 0.0f,10.0f,20.0f });
+			cObject2WT_.SetRotate({ 0.0f,0.0f,0.0f });
+			cObject_->RegistWorldTransform(&cObject2WT_);
+			collider2_ = std::make_unique<ObjectCollider>(cObject_.get());
+			auto* collider2 = dynamic_cast<ObjectCollider*>(collider2_.get());
+			collider2->SetCollisionAttribute(CollisionAttribute::Enemy);
+			collider2->SetWorldTransform(&cObject2WT_);
+			collider2->SetOffset({ 0.0f,1.0f,0.0f });
+			collider2->SetOBBSize({ 2.0f,2.0f,2.0f });
+		}
+
 		back_ = std::make_unique<Object3d>();
 		back_->Initialize(ShapeTag{}, Object3dManager::GetInstance()->GenerateName("Back"), Shape::ShapeKind::kPlane);
 		back_->SetTexture(TextureManager::GetInstance()->LoadTexture("uvChecker.png"));
@@ -111,6 +142,13 @@ namespace Norm {
 		camera_->DebugWithImGui();
 		//ポストエフェクト
 		PostEffectManager::GetInstance()->DebugWithImGui();
+
+		//当たり判定の可視化
+		cObject_->Debug(L"当たり判定オブジェクト");
+		auto* collider1 = dynamic_cast<ObjectCollider*>(collider1_.get());
+		collider1->Debug();
+		auto* collider2 = dynamic_cast<ObjectCollider*>(collider2_.get());
+		collider2->Debug();
 
 
 #endif // _DEBUG
