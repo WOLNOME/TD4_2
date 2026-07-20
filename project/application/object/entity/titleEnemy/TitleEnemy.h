69f08dbf667ff56b7cefb20d1de0b7c4a3a472d7
NagaiKohaku <k023g0037@g.neec.ac.jp>
2026-07-20 12:22:30 +0900

    タイトル演出用のエネミーを追加

---
+++ project/application/object/entity/titleEnemy/TitleEnemy.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "engine/3d/object/Object3d.h"
+#include "engine/3d/worldTransform/WorldTransform.h"
+#include "engine/3d/light/PointLight.h"
+
+#include <memory>
+
+class TitleEnemy {
+public:
+
+	void Initialize(Norm::Vector3 _pos);
+
+	void Update();
+
+	void DebugWithImGui();
+
+	Norm::PointLight* GetPointLight() const { return pointLight_.get(); }
+
+	void SetDirection(Norm::Vector3 _direction) { direction_ = _direction; }
+
+	void SetIsRun(bool const flag) { isRun_ = flag; }
+
+private:
+
+	std::unique_ptr<Norm::Object3d> object_;
+
+	Norm::WorldTransform wt_;
+
+	std::unique_ptr<Norm::PointLight> pointLight_;
+
+	Norm::Vector3 direction_ = { 1.0f,0.0f,0.0f };
+
+	float speed_ = 0.0f;
+
+	float minSpeed_ = 0.05f;
+
+	float maxSpeed_ = 0.1f;
+
+	float frequency_ = 0.0f;
+
+	float minFrequency_ = 2.0f;
+
+	float maxFrequency_ = 3.0f;
+
+	float maxLength_ = 45.0f;
+
+	float timer_ = 0.0f;
+
+	float startPosY_ = 0.0f;
+
+	bool isRun_ = false;
+};
+
--- project/engine/scene/TitleScene.h
+++ project/engine/scene/TitleScene.h
@@ -7,6 +7,8 @@
 #include <WorldTransform.h>
 
 // Application
+#include "application/object/entity/titleEnemy/TitleEnemy.h"
+
 #include <memory>
 
 namespace Norm {
@@ -68,13 +70,24 @@ namespace Norm {
 		//点光源
 		std::unique_ptr<PointLight> pointLight_;
 
-		std::unique_ptr<Object3d> object_ = nullptr;
+		std::unique_ptr<Object3d> backGroundObject_ = nullptr;
 		WorldTransform wt_;
+
+		std::vector<std::unique_ptr<TitleEnemy>> titleEnemies_;
 
 		UI titleUI_;
 
 		UI buttonUI_;
 
 		Vector2 initSize_;
+
+		int maxEnemy_ = 5;
+
+		bool isSceneChange_ = false;
+
+		float sceneChangeTimer_ = 0.0f;
+
+		float sceneChangeMaxTime_ = 2.0f;
+
 	};
 }
