dbd2365dfeaa3f74d2759704f359b6bc2ba49d29
WOLNOME <k023g0109@g.neec.ac.jp>
2026-07-23 16:59:13 +0900

    ステージ書き換え

--- project/application/object/entity/enemy/BaseEnemy.h
+++ project/application/object/entity/enemy/BaseEnemy.h
@@ -247,6 +247,7 @@ private:
 	bool isAreaColliding_ = false;
 	bool isBodyColliding_ = false;
 	bool isPlayerColliding_ = false;
+	bool isFootColliding_ = false;
 
 	// 回転中かどうかのフラグ
 	bool isRotating_ = false; 
